a38caffaf0fd8c9a98d9d807d2e1d47281dc032e
ratihjulistina <ratihjulistina@gmail.com>
2024-11-28 20:15:45 +0700

    feat:change font heading

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           height={38}
           priority
         /> */}
-        <h1 className="text-4xl text-bold">Test Deployment by Ratih</h1>
+        <h1 className="text-4xl font-bold">Test Deployment by Ratih</h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
